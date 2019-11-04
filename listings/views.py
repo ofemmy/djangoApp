c48154024c7ae0a09cddd5e0254dd53597ce32ca
Oladayo Fagbemi <oofagbemi@outlook.com>
2019-11-04 02:19:58 +0100

    added the listings ui and the realtors app

---
+++ listings/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, "listings/listings.html")
+
+
+def listing(request):
+    return render(request, "listings/listing.html")
+
+
+def search(request):
+    return render(request, "listings/search.html")
